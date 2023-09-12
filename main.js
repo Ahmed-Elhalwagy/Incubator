03c09e96d05f505a80cdd42db90b5d1fe1efcfd3
Ahmed-Elhalwagy <ahmed.elhalwagy88@gmail.com>
2023-09-12 08:29:35 +0300

    add contact-us, application, login, sign-up pages

--- main.js
+++ main.js
@@ -16,3 +16,44 @@ const application = document.querySelector('.application');
 application.addEventListener("click",()=>{
     window.location.href = "#"
 })
+
+// SIGN-UP
+const name = document.getElementsByName("name")[0];
+const email = document.getElementsByName("email")[0];
+const password = document.getElementsByName("password")[0];
+const profession = document.getElementsByName("profession")[0];
+
+const errorEl = document.getElementById("error");
+const sign_up_form = document.getElementById("sign_up")
+
+const errors = []
+
+sign_up_form.addEventListener('submit', e => {
+    errors = []
+    e.preventDefault();
+
+    if (name.value === "" || name.value == null) {
+        errors.push("Name is Required")
+    }
+    if (email.value === "" || email.value == null) {
+        errors.push("Email is Required")
+    }
+    if (password.value === "" || password.value == null) {
+        errors.push("Password is Required")
+    }
+    if (password.value.length < 6) {
+        errors.push("Password Must be longer than 6 characters")
+    }
+    if (password.value.length > 20) {
+        errors.push("Password Must be less than 20 characters")
+    }
+    if (profession.value === "" || profession.value == null) {
+        errors.push("Profession is Required")
+    }
+
+    if (errors.length > 0) {
+        errorEl.innerText = errors.join(", ")
+    }
+
+})
+
